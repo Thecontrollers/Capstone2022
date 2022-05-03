a2452da97679a620f949f7b8efdf0542e0bc69ef
Thecontrollers <mandudegaming@gmail.com>
2022-05-03 16:02:31 -0400

    Teams/Staff

--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,18 +1,60 @@
 import React from 'react';
 import '../../App.css';
 import './Staff.css'
+import Footer from '../Footer';
 
 export default function Staff() {
     return (
+    <>
     <div className='header_wrapper'>
     <h1 className='staff'>President</h1>
-    <div className='person'>
-        Person Name
-    </div>
+    <div className='person'>Sierra Pena </div>
     <div className='grade'>xx.</div>
     <div className='email'>
         <a href='xxx@sunypoly.edu'>xxx@sunypoly.edu</a>
     </div>
     </div>
+    <div className='header_wrapper'>
+    <h1 className='staff'>Game Coordinator</h1>
+    <div className='person'>PJ Farrell </div>
+    <div className='grade'>xx.</div>
+    <div className='email'>
+        <a href='xxx@sunypoly.edu'>xxx@sunypoly.edu</a>
+    </div>
+    </div>
+    <div className='header_wrapper'>
+    <h1 className='staff'>Team Manager</h1>
+    <div className='person'>Quinn Mahoney </div>
+    <div className='grade'>xx.</div>
+    <div className='email'>
+        <a href='xxx@sunypoly.edu'>xxx@sunypoly.edu</a>
+    </div>
+    </div>
+    <div className='header_wrapper'>
+    <h1 className='staff'>Event Coordinator</h1>
+    <div className='person'>Trey Yarwood </div>
+    <div className='grade'>xx.</div>
+    <div className='email'>
+        <a href='xxx@sunypoly.edu'>xxx@sunypoly.edu</a>
+    </div>
+    </div>
+    <div className='header_wrapper'>
+    <h1 className='staff'>Graphic Designer</h1>
+    <div className='person'>Long Tran </div>
+    <div className='grade'>xx.</div>
+    <div className='email'>
+        <a href='xxx@sunypoly.edu'>xxx@sunypoly.edu</a>
+    </div>
+    </div>
+    <div className='header_wrapper'>
+    <h1 className='staff'>Treasurer</h1>
+    <div className='person'>Andrew P.</div>
+    <div className='grade'>xx.</div>
+    <div className='email'>
+        <a href='xxx@sunypoly.edu'>xxx@sunypoly.edu</a>
+    </div>
+    </div>
+    <Footer />
+    </>
     );
 }
--- src/components/pages/Teams.js
+++ src/components/pages/Teams.js
@@ -10,48 +10,275 @@ import "./Teams.css";
 
 export default function Teams() {
   return (
-    <div>
-      <h1 className="teams">Teams</h1>
-      <div className="TeamContainer">
-        <div className="team_1">
-          Rocket League
-          <div className="team_1_wrapper">
-            Rocket League A<div className="record">(8-2)</div>
-            <ul className="player_name">
-              <li>Stephen Gardner (Captain)</li>
-              <li>Trey Yarwood (Starter)</li>
-              <li>Andrew Bushey (Starter)</li>
-              <li>Xander Wilson (Sub)</li>
-            </ul>
-          </div>
-          <div className="team_2">
-            Rocket League
-            <div className="team_2_wrapper">
-              Rocket League B<div className="record">(6-4)</div>
-              <ul className="player_name">
-                <li>Brandon McChain (Captain)</li>
-                <li>Joshua Corelli (Starter)</li>
-                <li>Louis Desarro (Starter)</li>
-              </ul>
-            </div>
-            <div className="team_3">
-              Overwatch
-              <div className="team_1_wrapper">
-                Overwatch A<div className="record">(7-3)</div>
-                <ul className="player_name">
-                  <li>Marco Demelerio (DPS / Captain)</li>
-                  <li>Donnell Anderson (DPS)</li>
-                  <li>Joshua Sudduth (Support)</li>
-                  <li>Tyler Lingen (Support)</li>
-                  <li>Randall Wheeler (Tank)</li>
-                  <li>Matthew Woodin (Tank)</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+     <>
+    <h1 className='teams'>Teams</h1>
+    <div className='TeamContainer'>
+    <div className="team_1">
+         Overwatch
+        <div className = "team_1_wrapper">
+        Team A
+        <div className = "record">
+            (0-0)
         </div>
-        <Footer />
-      </div>
+        <ul className='player_name'>
+            <li> Marco Demeterio
+                <span className="role"> Captain/DPS</span>
+            </li>
+            <li> Donnell Anderson 
+            <span className="role"> DPS</span>
+            </li>
+            <li> Joshua Sudduth 
+            <span className="role"> Support</span>
+            </li>
+            <li> Tyler Lingden 
+            <span className="role"> Support</span>
+            </li>
+            <li> Randall Wheeler 
+            <span className="role"> Tank</span>
+            </li>
+            <li> Matthew Woodin 
+            <span className="role"> Tank</span>
+            </li>
+            <li> Tsering Dhondup 
+            <span className="role"> Sub</span>
+            </li>
+            <li> Cale Norcross
+            <span className="role"> Sub</span>
+            </li>
+            <li> Gregory Patti
+            <span className="role"> Sub</span>
+            </li>
+            <li> Brent Orandi
+            <span className="role"> Sub</span>
+            </li>
+        </ul>
     </div>
+    <div className = "team_1_wrapper">
+        Team B
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Marco Demeterio
+                <span className="role"> Captain/DPS</span>
+            </li>
+            <li> Joshua Sudduth 
+            <span className="role"> DPS</span>
+            </li>
+            <li> Tsering Dhondup 
+            <span className="role"> Support</span>
+            </li>
+            <li> Gregory Patti
+            <span className="role"> Support</span>
+            </li>
+            <li> Cale Norcross
+            <span className="role"> Tank</span>
+            </li>
+            <li> Brent Orandi
+            <span className="role"> Tank</span>
+            </li>
+            <li> Matthew Woodin 
+            <span className="role"> Sub</span>
+            </li>
+            <li> Tyler Lingden 
+            <span className="role"> Sub</span>
+            </li>
+            <li> Donnell Anderson 
+            <span className="role"> Sub</span>
+            </li>
+            <li> Randall Wheeler 
+            <span className="role"> Sub</span>
+            </li>
+        </ul>
+    </div>
+    </div> 
+    <div className="team_1">
+         Rocket League
+        <div className = "team_1_wrapper">
+        Team A
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Stephen Gardner
+                <span className="role"> Captain</span>
+            </li>
+            <li> Trey yarwood
+            </li>
+            <li> Andrew Bushey 
+            </li>
+            <li> Xander Wilson 
+            <span className="role"> Sub</span>
+            </li>
+        </ul>
+    </div>
+    <div className = "team_1_wrapper">
+        Team A
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Brendan Mchain
+                <span className="role"> Captain</span>
+            </li>
+            <li> Joshua Corelli
+            </li>
+            <li> Louis DeSarro 
+            </li>
+        </ul>
+    </div>
+    </div>
+    <div className="team_1">
+         League of Legends
+        <div className = "team_1_wrapper">
+        Team A
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Alessandro Cellamare
+                <span className="role"> Top</span>
+            </li>
+            <li> Joe Center
+            <span className="role"> Jungle/Captain</span>
+            </li>
+            <li> Andrew Bushey 
+            <span className="role"> Mid</span>
+            </li>
+            <li> Xander Wilson 
+            <span className="role"> Bottem</span>
+            </li>
+            <li> Xander Wilson 
+            <span className="role"> Support</span>
+            </li>
+            <li> Xander Wilson 
+            <span className="role"> Sub</span>
+            </li>
+        </ul>
+    </div>
+    </div>
+    <div className="team_1">
+         Valorant
+        <div className = "team_1_wrapper">
+        Team A
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Patrick Farrell
+                <span className="role"> Captain</span>
+            </li>
+            <li>Philip Perkowski </li>
+            <li> Lucas McHale  </li>
+            <li> Andrew Zaw  </li>
+            <li> Nate Wojcinski  </li>
+            <li> Nishant Sharma  </li>
+            <li> David Neri  </li>
+        </ul>
+    </div>
+    </div>
+    <div className="team_1">
+         Smash Ulimate
+        <div className = "team_1_wrapper">
+        Team A
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Jason Guzman
+                <span className="role"> Captain</span>
+            </li>
+            <li>Giancarlo Pimentel </li>
+            <li> Sharmar Stoddard  </li>
+            <li> Bennett Ketchum  
+            <span className="role"> Sub</span>
+            </li>
+        </ul>
+    </div>
+    <div className = "team_1_wrapper">
+        Team B
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Tyrese Del Valle
+                <span className="role"> Captain</span>
+            </li>
+            <li>Matthew Steves </li>
+            <li> Patrick Amarasinghe </li>
+            <li> Mitchel Clow  
+            <span className="role"> Sub</span>
+            </li>
+        </ul>
+    </div>
+    <div className = "team_1_wrapper">
+        Team C
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Tony Fedele
+                <span className="role"> Captain</span>
+            </li>
+            <li> Marcus Camacho  </li>
+            <li> Javier Crespo-Law  </li>
+            <li> Nicholas Mineo  
+            <span className="role"> Sub</span>
+            </li>
+        </ul>
+    </div>
+    <div className = "team_1_wrapper">
+        Team D
+        <div clDssName = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li> Owen Drummond
+                <span className="role"> Captain</span>
+            </li>
+            <li> Nicholas Mezzapella </li>
+            <li> LeAndre Hercules  </li>
+            <li> Noah Thompson  
+            <span className="role"> Sub</span>
+            </li>
+        </ul>
+    </div>
+    </div>
+    <div className="team_1">
+            Hearthstone
+        <div className = "team_1_wrapper">
+        Team A
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li>Ryan Bassett </li>  
+        </ul>
+    </div>
+    </div>
+    <div className="team_1">
+            Rainbow Six Siege
+        <div className = "team_1_wrapper">
+        Team A
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li>Matthew Pikowsky 
+            <span className="role"> Captain</span>
+            </li>  
+            <li>Justin Virga </li>  
+            <li>Andrew Bardin </li>  
+            <li>J William Bowen </li>  
+            <li>Hunter McGivern </li>  
+            <li>Hunter O'Gorman 
+            <span className="role"> Sub</span>
+            </li>  
+        </ul>
+    </div>
+    </div>
+    </div>
+    <Footer/> 
+    </>
   );
 }
