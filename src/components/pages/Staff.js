01d36d37316c0852329cd45c5a9e60c28d1d6e55
Thecontrollers <mandudegaming@gmail.com>
2022-05-03 05:25:19 -0400

    COMING SOON

--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import '../../App.css';
+import './Staff.css'
 
 export default function Staff() {
-    return <h1 className='Staff'>Staff</h1>
+    return (
+    <div className='header_wrapper'>
+    <h1 className='staff'>President</h1>
+    <div className='person'>
+        Person Name
+    </div>
+    <div className='grade'>xx.</div>
+    <div className='email'>
+        <a href='xxx@sunypoly.edu'>xxx@sunypoly.edu</a>
+    </div>
+    </div>
+    );
 }
--- src/components/pages/Teams.js
+++ src/components/pages/Teams.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import '../../App.css';
+import Footer from '../Footer';
+import './Teams.css';
+
+// Rocket League
+//         Hearthstone
+//         Super Smash Bros. Ulimate
+//         League of Legends
+//         Overwatch
+//         Rainbow Six Siege
 
 export default function Teams() {
-    return <h1 className='teams'>Teams</h1>
+    return (
+    <>
+    <h1 className='teams'>Teams</h1>
+    <div className='TeamContainer'>
+    <div className="team_1">
+         Rocket League
+        <div className = "team_1_wrapper">
+        Team 1
+        <div className = "record">
+            (0-0)
+        </div>
+        <ul className='player_name'>
+            <li>
+                Player Name
+            </li>
+            <li>
+                Player Name
+            </li>
+            <li>
+                Player Name
+            </li>
+        </ul>
+    </div>
+    </div>
+    </div>
+    <Footer/> 
+    </>
+    );
 }
